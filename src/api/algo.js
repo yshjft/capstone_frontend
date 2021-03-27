223a50515e3afc89d6d1733fab6f32c7ed8ec7b5
hong yuseok <jericoh@naver.com>
2021-03-27 19:25:53 +0900

    api url 변경

--- src/api/algo.js
+++ src/api/algo.js
@@ -2,7 +2,7 @@ import api from './index'
 
 export async function getAlgoPosts() {
   try {
-    const res = await api.get('/algos')
+    const res = await api.get('/post')
     return res.data
   } catch (error) {
     throw error
