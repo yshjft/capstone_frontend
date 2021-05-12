7cd11b5c1470027dca0a2f1a241521df3e2b4b17
hong yuseok <jericoh@naver.com>
2021-05-13 01:44:19 +0900

    postPreview 컴포넌트에 메모 미리보기 추가

--- src/components/PostPreview/PostPreview.jsx
+++ src/components/PostPreview/PostPreview.jsx
@@ -5,7 +5,7 @@ import {faLock, faThumbsUp} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import language from '../../lib/language'
 import {formatDate} from '../../lib/formatDate'
-import style from './index.module.scss'
+import styles from './index.module.scss'
 
 const PostPreview = (props) => {
   const {postInfo} = props
@@ -16,29 +16,32 @@ const PostPreview = (props) => {
   }
 
   return (
-    <div className={style.postInfo} onClick={handleClick}>
+    <div className={styles.postInfo} onClick={handleClick}>
       <Card>
-        <Card.Header className={style.header}>{postInfo.title}</Card.Header>
+        <Card.Header className={styles.header}>{postInfo.title}</Card.Header>
         <Card.Body>
-          <div className={style.dateLike}>
-            <span className={style.date}>{formatDate(postInfo.createdAt)}</span>
-            <span className={style.like}>
-              <FontAwesomeIcon icon={faThumbsUp} className={style.icon} />
+          <div className={styles.dateLike}>
+            <span className={styles.date}>{formatDate(postInfo.createdAt)}</span>
+            <span className={styles.like}>
+              <FontAwesomeIcon icon={faThumbsUp} className={styles.icon} />
               <span>{postInfo.likeNum}</span>
             </span>
           </div>
-          <Badge variant="primary" className={style.badge}>
+          <Badge variant="primary" className={styles.badge}>
             {language(postInfo.language)}
           </Badge>
-          <div className={style.byWriter}>
+          <div className={styles.byWriter}>
             <span>by </span>
-            <span className={style.writer}>{`${postInfo.writer}`}</span>
+            <span className={styles.writer}>{`${postInfo.writer}`}</span>
             {postInfo.public === 0 && (
-              <span className={style.private}>
-                <FontAwesomeIcon icon={faLock} className={style.icon} />
+              <span className={styles.private}>
+                <FontAwesomeIcon icon={faLock} className={styles.icon} />
                 <span>비공개</span>
               </span>
             )}
+          </div>
+          <div className={styles.memo}>
+            {postInfo.memoLength === postInfo.memo.length ? postInfo.memo : postInfo.memo + '...'}
           </div>
         </Card.Body>
       </Card>
